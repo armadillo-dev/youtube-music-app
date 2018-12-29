97bf68322a81be4c38040028bda1ace574107129
armadillo-dev <armadillo.dev167@gmail.com>
2018-12-29 15:16:02 +0900

    the first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    title: 'Youtube Music',
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, `assets/favicon.${process.platform === 'darwin' ? 'icns' : 'ico'}`),
+  });
+  console.log(path.join(__dirname, `assets/favicon.${process.platform === 'darwin' ? 'icns' : 'ico'}`));
+  mainWindow.loadURL('https://music.youtube.com');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
