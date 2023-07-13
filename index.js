1ad58a7767590838ea6eb3de03d23c27b68ea11c
Sejong Yoo <ysj7705@gmail.com>
2023-07-13 10:51:04 +0900

    add tray feature

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, BrowserWindow, Menu, Tray} = require('electron');
 const path = require('path');
 
 let mainWindow;
+let appTray;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -17,14 +18,50 @@ function createWindow() {
 
   mainWindow.loadURL('https://music.youtube.com');
 
+  mainWindow.on('close', (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
   mainWindow.on('closed', function () {
     mainWindow = null;
+  });
+}
+
+function createTray() {
+  if (appTray) {
+    return;
+  }
+
+  const trayIconPath = path.join(
+    __dirname,
+    `assets/favicon.${process.platform === 'darwin' ? 'icns' : 'ico'}`,
+  );
+
+  const trayMenu = Menu.buildFromTemplate([
+    {label: 'Open', click: () => mainWindow.show()},
+    {
+      label: 'Quit',
+      click: () => {
+        appTray.destroy();
+        app.quit();
+      },
+    },
+  ]);
+
+  appTray = new Tray(trayIconPath);
+  appTray.setToolTip('Youtube Music');
+  appTray.setContextMenu(trayMenu);
+
+  appTray.on('click', () => {
+    mainWindow.show();
   });
 }
 
 app.on('ready', () => {
   Menu.setApplicationMenu(null);
   createWindow();
+  createTray();
 });
 
 app.on('window-all-closed', function () {
