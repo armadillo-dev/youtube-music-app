15fa8291d965b1c9e8ed50bb8fc73a054b679568
Sejong Yoo <ysj7705@gmail.com>
2023-07-13 10:40:28 +0900

    remove menubar

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     title: 'Youtube Music',
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, `assets/favicon.${process.platform === 'darwin' ? 'icns' : 'ico'}`),
+    icon: path.join(
+      __dirname,
+      `assets/favicon.${process.platform === 'darwin' ? 'icns' : 'ico'}`,
+    ),
+    frame: true,
   });
 
   mainWindow.loadURL('https://music.youtube.com');
@@ -18,7 +22,10 @@ function createWindow () {
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  Menu.setApplicationMenu(null);
+  createWindow();
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
